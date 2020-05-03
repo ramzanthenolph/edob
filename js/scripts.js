bfb8be789b3cc2dfdee41cb7482a62945c48f80c
ramzan <ramzanthenolph@gmail.com>
2020-05-03 18:10:29 +0300

    add cheat sheet paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,128 @@
 function edoban(){
-var calculate = function(date, month, year, century){
-    var yearcode = function( year){
-        return (year + (year/4))% 7;
-    }
-    var arrayMonth = [0,3,3,6,1,4,6,2,5,0,3,5];
-    var monthCode = function(month){
+    
+    
+}
+
+    function mm(){
+        var month = parseInt(document.getElementById("month").value);
+        var arrayMonth = [0,3,3,6,1,4,6,2,5,0,3,5];
         if(month ==1){
-            return arrayMonth[0];
+            var monthCode= arrayMonth[0];
+            return monthCode;
         }
         else if(month ==2){
-            return arrayMonth[1];
+            monthCode= arrayMonth[1];
+            return monthCode
         }
         else if(month == 3){
-            return arrayMonth[2];
+            monthCode= arrayMonth[2];
+            return monthCode;
         }
         else if (month == 4){
-            return arrayMonth[3];
+            monthCode = arrayMonth[3];
+            return monthCode;
         }
         else if (month == 5){
-            return arrayMonth[4];
+            monthCode = arrayMonth[4];
+            return monthCode;
         }
         else if (month == 6){
-            return arrayMonth[5];
+            monthCode = arrayMonth[5];
+            return monthCode;
         }
         else if(month == 7){
-            return arrayMonth[6];
+            monthCode= arrayMonth[6];
+            return monthCode;
         }
         else if (month == 8){
-            return arrayMonth[7];
+            monthCode = arrayMonth[7];
+            return monthCode;
         }
         else if (month == 9){
-            return arrayMonth[8];
+            monthCode = arrayMonth[8];
+            return monthCode;
         }
         else if (month == 10){
-            return arrayMonth[9];
+            monthCode = arrayMonth[9];
+            return monthCode;
         }
         else if (month == 11){
-            return arrayMonth[10];
-        }
-        else(month == 12){
-            return arrayMonth[11];
-        }
-    }
-    var centurycode = function(century){
-        if (century == 17){
-            return 4;
-        }
-        else if(century == 18){
-            return 2;
-        }
-        else if(century == 19){
-            return 0;
-        }
-        else if(century == 20){
-            return 6;
-        }
-        else if (century == 21){
-            return 4;
-        }
-        else if(century == 22){
-            return 2;
+            monthCode = arrayMonth[10];
+            return monthCode;
         }
         else{
-            return 0;
+            monthCode= arrayMonth[11];
+            return monthCode;
         }
     }
+        function cc(){
+        var century = parseInt(document.getElementById("century").value);
+        if (century == 17){
+            var centurycode= 4;
+            return centurycode;
 
-    if(year/=4 && year/= 400)
-    return ( ( (century/4) - 2 * century - 1) + ((5 * year/4) ) + ((26 * (month + 1 ) /10)) + date ) % 7;
+        }
+        else if(century == 18){
+            centurycode= 2;
+            return centurycode;
+        }
+        else if(century == 19){
+            centurycode= 0;
+            return centurycode;
+        }
+        else if(century == 20){
+            centurycode = 6;
+            return centurycode;
+        }
+        else if (century == 21){
+            centurycode = 4
+            return centurycode;
+        }
+        else if(century == 22){
+            centurycode= 2;
+            return centurycode;
+        }
+        else{
+            centurycode = 0
+            return centurycode;
+        }
     }
-    // This is the  formula used  for finding the day of the week.The output expected should range 0 - 6
-    var date = parseInt(document.getElementById("date").value);
-    var month = parseInt(document.getElementById("month").value);
-    var year = parseInt(document.getElementById("year").value);
-    var century = parseInt(document.getElementById("century").value);
-    var dayOfTheWeek = calculate(date, month, year, century);
-    alert(dayOfTheWeek);
+     function calculate (date, yearcode, monthCode, centurycode, year){
 
-    /*var gdr = document.getElementById("one").value;
+        var date = parseInt(document.getElementById("date").value);
+        var year = parseInt(document.getElementById("year").value);
+        var yearcode = (year + (year/4))% 7;
+        yearcode = Math.trunc(yearcode);
+        if(year % 4 ==0){
+            var dayOfTheWeek= (yearcode + mm(monthCode) + cc(centurycode) + date - 1) % 7;
+            alert( dayOfTheWeek);
+        }
+        else{
+            dayOfTheWeek= (yearcode + mm(monthCode) + cc(centurycode) + date )% 7;
+            alert( dayOfTheWeek);
+        }
+    }
+    
+    
+    var gdr = document.getElementById("one").value;
      //Try get document by name.same name for each gender
-    if(gdr[0] == true){
-        if(dayOfTheWeek==0){
+    if(gdr[0].checked == true){
+        if( calculate(dayOfTheWeek)==0){
             console.log("Your name is Kwasi");
         }
-        else if(dayOfTheWeek ==1){
+        else if(calculate(dayOfTheWeek) ==1){
             console.log("Your name is Kwadwo");
         }
-        else if(dayOfTheWeek ==2){
+        else if(calculate(dayOfTheWeek) ==2){
             console.log("Your name is Kwabena");
         }
-        else if(dayOfTheWeek == 3){
+        else if(calculate(dayOfTheWeek) == 3){
             console.log("Your name is Kwaku");
         }
-        else if(dayOfTheWeek == 4){
+        else if(calculate(dayOfTheWeek) == 4){
             console.log("Your name is Yam");
         }
-        else if(dayOfTheWeek == 5){
+        else if(calculate(dayOfTheWeek) == 5){
             console.log("Your name is Kofi");
         }
         else{
@@ -127,5 +154,4 @@ var calculate = function(date, month, year, century){
     }
     else {
         alert("Input gender");
-    }*/
-}
+    }

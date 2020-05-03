43c47934c9056700f3d38f072cd92a5bc37b5d23
ramzan <ramzanthenolph@gmail.com>
2020-05-03 14:21:05 +0300

    add new method to calculate day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,131 @@
-var  century ;
-var year;
-var month;
-var date;
-var dayOfTheWeek ;
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
+function edoban(){
+var calculate = function(date, month, year, century){
+    var yearcode = function( year){
+        return (year + (year/4))% 7;
+    }
+    var arrayMonth = [0,3,3,6,1,4,6,2,5,0,3,5];
+    var monthCode = function(month){
+        if(month ==1){
+            return arrayMonth[0];
+        }
+        else if(month ==2){
+            return arrayMonth[1];
+        }
+        else if(month == 3){
+            return arrayMonth[2];
+        }
+        else if (month == 4){
+            return arrayMonth[3];
+        }
+        else if (month == 5){
+            return arrayMonth[4];
+        }
+        else if (month == 6){
+            return arrayMonth[5];
+        }
+        else if(month == 7){
+            return arrayMonth[6];
+        }
+        else if (month == 8){
+            return arrayMonth[7];
+        }
+        else if (month == 9){
+            return arrayMonth[8];
+        }
+        else if (month == 10){
+            return arrayMonth[9];
+        }
+        else if (month == 11){
+            return arrayMonth[10];
+        }
+        else(month == 12){
+            return arrayMonth[11];
+        }
+    }
+    var centurycode = function(century){
+        if (century == 17){
+            return 4;
+        }
+        else if(century == 18){
+            return 2;
+        }
+        else if(century == 19){
+            return 0;
+        }
+        else if(century == 20){
+            return 6;
+        }
+        else if (century == 21){
+            return 4;
+        }
+        else if(century == 22){
+            return 2;
+        }
+        else{
+            return 0;
+        }
+    }
 
-
-
-// This is what we expect to be outputed
-// initialise empty variables to be used in our function.Their names correspond to our ids in HTML.
-
-
-
-var day = function(){
-    date = parseInt(document.getElementById("date").value);
-    month = parseInt(document.getElementById("month").value);
-    year = parseInt(document.getElementById("year").value);
-    century = parseInt(document.getElementById("century").value);
-    
-}
-
-
-
-function calculate(){
-    day();   // call the above function (day)
-    dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
-    return(Math.floor(dayOfTheWeek));
-    console.log(dayOfTheWeek);
+    if(year/=4 && year/= 400)
+    return ( ( (century/4) - 2 * century - 1) + ((5 * year/4) ) + ((26 * (month + 1 ) /10)) + date ) % 7;
+    }
     // This is the  formula used  for finding the day of the week.The output expected should range 0 - 6
-}
+    var date = parseInt(document.getElementById("date").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var century = parseInt(document.getElementById("century").value);
+    var dayOfTheWeek = calculate(date, month, year, century);
+    alert(dayOfTheWeek);
 
- var result = function(){
-dayOfTheWeek = calculate();
-checkgender();
-}
-// This code checks the gender input.It gives an alert with a message "input gender" if gender input is not found
-var checkgender = function() {
-    calculate();
-    var gdr = document.getElementsByName("one"); //Try get document by name.same name for each gender
-    if(gdr[0].checked == true){
-        gender = "male";
-    }else if(gdr[1].checked == true ){
-        gender = "female";
+    /*var gdr = document.getElementById("one").value;
+     //Try get document by name.same name for each gender
+    if(gdr[0] == true){
+        if(dayOfTheWeek==0){
+            console.log("Your name is Kwasi");
+        }
+        else if(dayOfTheWeek ==1){
+            console.log("Your name is Kwadwo");
+        }
+        else if(dayOfTheWeek ==2){
+            console.log("Your name is Kwabena");
+        }
+        else if(dayOfTheWeek == 3){
+            console.log("Your name is Kwaku");
+        }
+        else if(dayOfTheWeek == 4){
+            console.log("Your name is Yam");
+        }
+        else if(dayOfTheWeek == 5){
+            console.log("Your name is Kofi");
+        }
+        else{
+            console.log("Your name is kwame");
+        }
+    }
+    else if(gdr[1].checked == true ){
+        if(dayOfTheWeek==0){
+            console.log("Your name is Akasua");
+        }
+        else if(dayOfTheWeek ==1){
+            console.log("Your name is Adwa");
+        }
+        else if(dayOfTheWeek ==2){
+            console.log("Your name is Abenna");
+        }
+        else if(dayOfTheWeek == 3){
+            console.log("Your name is Akua");
+        }
+        else if(dayOfTheWeek == 4){
+            console.log("Your name is Afua");
+        }
+        else if(dayOfTheWeek == 5){
+            console.log("Your name is Afua");
+        }
+        else{
+            console.log("Your name is Ama");
+        }
     }
     else {
-        alert("Input gender")
-    }
-    
-    switch(gender){
-        case "male":
-        if(dayOfTheWeek == 0){
-            document.getElementById("result").innerHTML = "Monday" + MaleNames[0];
-        }else if (dayOfTheWeek == 1){
-            document.getElementById("result").innerHTML = "Tuesday" + maleNames[1];
-        }else if (dayOfTheWeek == 2){ 
-            document.getElementById("result").innerHTML = "Wednesay" + maleNames[2];
-        }else if (dayOfTheWeek == 3){ 
-            document.getElementById("result").innerHTML = "Thursday" + maleNames[3];
-        }else if (dayOfTheWeek == 4){ 
-            document.getElementById("result").innerHTML = "Friday" + maleNames[4];
-        }else if (dayOfTheWeek == 5){ 
-            document.getElementById("result").innerHTML = "Saturday" + maleNames[5];
-        }else { 
-            document.getElementById("result").innerHTML = "Sunday" + maleNames[6];
-    }
-    break;
-    case "female":
-            if(dayOfTheWeek == 0){
-                document.getElementById("result").innerHTML = "Monday" + femaleNames[0];
-            }else if (dayOfTheWeek == 1){
-                document.getElementById("result").innerHTML = "Tuesday" + femaleNames[1];
-            }else if (dayOfTheWeek == 2){ 
-                document.getElementById("result").innerHTML = "Wednesay" + femaleNames[2];
-            }else if (dayOfTheWeek == 3){ 
-                document.getElementById("result").innerHTML = "Thursday" + femaleNames[3];
-            }else if (dayOfTheWeek == 4){ 
-                document.getElementById("result").innerHTML = "Friday" + femaleNames[4];
-            }else if (dayOfTheWeek == 5){ 
-                document.getElementById("result").innerHTML = "Saturday" + femaleNames[5];
-            }else{ 
-                document.getElementById("result").innerHTML = "Sunday" + femaleNames[6];
-            }
-    break;
-    default:
-        
-    }
-
-
-
+        alert("Input gender");
+    }*/
 }
-// The code above should be able to calculate the day from the date given and also get  gender
- 
-/*removed arrays*/
-
-    )
-    //         }
-    //     }
-    //     }
-    // }
-    // The code below should ouput Akan names if they run sucessfully.
-    
